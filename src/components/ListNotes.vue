b36d8f65330f8db6713c6020b1ea6f4b6ad65309
irina08 <38026611+irina08@users.noreply.github.com>
2019-11-24 15:54:21 -0800

    Add files via upload

---
+++ src/components/ListNotes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <note v-on:delete-note="deleteNote" v-for="note in notes" :note.sync="note"></note>
+  </div>
+</template>
+
+<script>
+  import Note from './Note';
+  export default {
+    props: ['notes'],
+    components: {
+      Note
+    },
+    methods: {
+      deleteNote(note) {
+        var removeIt = confirm('Do you really want to remove it?');
+        if (removeIt) {
+          const noteIndex = this.notes.indexOf(note);
+          this.notes.splice(noteIndex, 1);
+          //alert('Note deleted!');
+        }
+      }
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Note.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class='ui centered card ww'>
+    <div class="content" v-show="!showEditForm">
+      <div class='header'>
+          {{ note.title }}
+      </div>
+      <div class='meta italic'>
+          {{ note.description }}
+      </div>
+      <div class='extra content'>
+          <span class='right floated edit icon' v-on:click="showForm">
+          <i class='edit icon'></i>
+        </span>
+        <span class='right floated trash icon' v-on:click="deleteNote(note)">
+          <i class='trash icon'></i>
+        </span>
+      </div>
+    </div>
+    <div class="content" v-show="showEditForm">
+      <div class='ui form'>
+        <div class='field'>
+          <label>Title</label>
+          <input type='text' v-model="note.title" >
+        </div>
+        <div class='field'>
+          <label>Description</label>
+          <input type='text' v-model="note.description" >
+        </div>
+        <div class='ui two buttons'>
+          <button class='ui basic button' v-on:click="hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['note'],
+    data() {
+      return {
+        showEditForm: false,
+      }
+    },
+    methods: {
+      deleteNote(note) {
+        this.$emit('delete-note', note);
+      },
+      showForm() {
+        this.showEditForm = true;
+      },
+      hideForm() {
+        this.showEditForm = false;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .italic {
+    font-style: italic
+  }
+  @media (min-width: 768px) {
+  div.ww {
+      width: 75%;
+  }
+}
+  
+</style>
